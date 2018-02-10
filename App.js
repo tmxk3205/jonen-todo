886eb95408532c7b6e3a89e82dd369ac5c65876d
Woo <logic@Jaemuns-MacBook-Pro.local>
2018-02-10 15:33:56 -0800

    Add Basic Layout

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  TextInput,
+  Dimensions,
+  Platform
+} from 'react-native';
+
+const { height, width } = Dimensions.get('window');
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar barStyle="light-content" />
+        <Text style={styles.title}>Calstar To Do</Text>
+        <View style={styles.card}>
+          <TextInput style={styles.input} placeholder={'New To Do'} />
+        </View>
       </View>
     );
   }
@@ -14,8 +28,35 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f23657',
+    alignItems: 'center'
   },
+  title: {
+    color: 'white',
+    fontSize: 30,
+    marginTop: 50,
+    fontWeight: '200',
+    marginBottom: 30
+  },
+  card: {
+    backgroundColor: 'white',
+    flex: 1,
+    width: width - 30,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    ...Platform.select({
+      ios: {
+        shadowColor: '#232323',
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset: {
+          height: -1,
+          width: 0
+        }
+      },
+      android: {
+        elevation: 3
+      }
+    })
+  }
 });
